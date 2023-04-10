aa263743f5f723e0d6d4c4ec41561feb6b933c6f
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-04-10 15:59:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-//your JS code here. If required.
+let input = document.getElementById("ip");
+let btn = document.getElementById("btn");
+let output = document.getElementById("output")
+
+btn.addEventListener("click" , PromiseChaining)
+function PromiseChaining(){
+  const promise1 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(input.value);
+    }, 2000);
+  });
+
+  promise1
+    .then((result) => {
+      output.textContent = `Result: ${result}`;
+      return result;
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(result * 2);
+        }, 1000);
+      });
+    })
+    .then((result) => {
+      output.textContent = `Result: ${result}`;
+      return result;
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(result - 3);
+        }, 1000);
+      });
+    })
+    .then((result) => {
+      output.textContent = `Result: ${result}`;
+      return result;
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(result / 2);
+        }, 1000);
+      });
+    })
+    .then((result) => {
+      output.textContent = `Result: ${result}`;
+      return result;
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(result + 10);
+        }, 1000);
+      });
+    })
+    .then((result) => {
+      output.textContent = `Final Result: ${result}`;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
